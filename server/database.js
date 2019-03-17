e829a6d78758115b8a777405f9ba6f85ac8bc220
sbeleno <belenoshirley@hotmail.com>
2019-03-17 18:06:09 -0400

    Add uri mongodb production

--- server/database.js
+++ server/database.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const URI = 'mongodb://localhost/mean-crud';
+//const URI = 'mongodb://localhost/mean-crud';
+const URI = 'mongodb+srv://shirley:shirley140@cluster0-acdb4.mongodb.net/test?retryWrites=true';
 
 mongoose.connect(URI)
     .then(db => console.log('DB is connected'))
