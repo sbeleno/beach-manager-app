1f8c42413b3128ddd51a988e436548320415d1a4
sbeleno <belenoshirley@hotmail.com>
2019-03-17 19:32:42 -0400

    Update path

--- server/index.js
+++ server/index.js
@@ -38,7 +38,7 @@ app.use('/api/employees', require('./routes/employee.routes'));
 app.use('/api/beaches', require('./routes/beach.routes'));
 
 app.use('*', (req, res, next) => {
-    res.sendFile(path.join(__dirname, './frontend/dist/frontend', 'index.html'))
+    res.sendFile('./frontend/dist/frontend/index.html')
 });
 
 
