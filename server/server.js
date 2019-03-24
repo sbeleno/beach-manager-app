6c4f206761cac67b3cdfd84df4eb44e4429633b0
sbeleno <belenoshirley@hotmail.com>
2019-03-24 11:02:55 -0400

    configuracion server

--- server/server.js
+++ server/server.js
@@ -9,8 +9,6 @@ const { mongoose } = require('./database');
 
 // Settings
 app.set('port', process.env.PORT || 3000);
-
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -18,45 +16,22 @@ app.use(bodyParser.json());
 app.use(morgan('dev'));
 //app.use(cors({origin: 'http://localhost:4200'}));
 app.use(cors());
-app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
-/*app.use((req, res) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+// Serve static assets if in production
+if (process.env.NODE_ENV ==='production') {
+    // Set stactic folder
+    app.use(express.static('frontend/dist'));
 
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT,POST,DELETE,GET');
-        return res.status(200).json({});
-    }
-});*/
-
-/*app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname , '../frontend/dist/frontend/index.html'));
-});*/
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname , 'frontend', 'dist/frontend', 'index.html'));
+    });
+}
 
 // Routes
 app.use('/api/signup', require('./routes/user.routes'));
 app.use('/api/login', require('./routes/login.routes'));
 app.use('/api/employees', require('./routes/employee.routes'));
 app.use('/api/beaches', require('./routes/beach.routes'));
-
-
-
-app.use((req, res, next) => {
-    const error = new Error('Not found');
-    error.status = 404;
-    next(error);
-});
-
-app.use((error, req, res) => {
-    res.status(error.status || 500);
-    res.json({
-        error: {
-            message: error.message
-        }
-    });
-});
 
 
 app.listen(app.get('port'), () => {
